dc5a97aa6f49e05a4f13c9e08b8d929581a01de2
Aj <62760393+AjAj1@users.noreply.github.com>
2020-05-29 22:10:31 +0100

    Update Forwarding Tester.py

--- Forwarding Tester.py
+++ Forwarding Tester.py
@@ -9,14 +9,23 @@ Password = '' # Insert Your Email Password Here
 
 # Script:
 	
-def Test():
+Quantity = input ('Quantity of Tasks: ')
+Quantity = int(Quantity)
+print('-------------')
+count = 0
+while (count < Quantity):
+
+	count = count + 1
+
 	now = datetime.now()
 	current_time = now.strftime("%H:%M:%S")
+	
+	Subject = 'Email Successfully Received - '+str(count)
 	
 	try:
 		f=open('Emails.txt')
 		line=f.readlines()
-		Email = line[0]
+		Email = line[count]
 	except IndexError:
 		print('')
 		print('['+current_time+']'+ Fore.YELLOW + ' All Emails Tested!')
@@ -24,28 +33,16 @@ def Test():
 
 	mail = {
 		'from':'Forwarding Success! <mymail@foo.com>',
-		'subject': 'Email Successfully Received.',
+		'subject': Subject,
 		'content_text': 'This was a test to ensure your emails are forwarding correctly. Made By Aj#0069', 
 	}
-	
+		
 	try:
 		server = zmail.server(Username, Password)
 		server.send_mail(Email, mail)
 
 	except:
 		print('['+current_time+']'+ Fore.YELLOW + ' Unable To Send Email!')
-	
+		
 	print('['+current_time+']'+ Fore.GREEN + ' Successfully Sent Email to: '+ Fore.WHITE +Email)
-	
-	with open("Emails.txt", "w") as outfile:
-		for pos, line in enumerate(line):
-			if pos != 0:
-				outfile.write(line)
-
-Quantity = input ('Quantity of Tasks: ')
-Quantity = int(Quantity)
-print('-------------')
-count = 0
-while (count < Quantity):
-	count = count + 1
-	Test()
+		
